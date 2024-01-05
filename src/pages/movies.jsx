a2453fb4d6eae9295d4ed323f0ee9bbbe45f5cb0
Giovanni <133590612+Giovanni-Serra@users.noreply.github.com>
2024-01-05 17:03:38 +0100

    Update movies.jsx

--- src/pages/movies.jsx
+++ src/pages/movies.jsx
@@ -123,7 +123,7 @@ function Movie() {
                                 <FormatQuoteIcon />
                                 </>) : ""}
                             </Typography>
-                            <Box sx={{display: "flex", gap: "10px", justifyContent: "center", marginBlock: "1rem, flex-wrap: wrap"}}>
+                            <Box sx={{display: "flex", gap: "10px", justifyContent: "center", marginBlock: "1rem, flexWrap: wrap"}}>
                             {genres ? genres.map(genre => {
                                 return <Typography 
                                     variant="h5"
